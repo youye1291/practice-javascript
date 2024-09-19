5a5cd571215e52f6e20f712bd231fad861fd9db3
youye1291 <nkhtyuuya11@gmail.com>
2024-09-19 05:22:02 +0000

    9月１９日jabaScriptの導入

---
+++ main.js
@@ -0,0 +1,17 @@
+document.querySelector("h1").textContent = "自分の名前"
+document.querySelector("h2").textContent = "中畑"
+
+//documentとは！！
+/*htmlファイルで書かれているコードの一つのオブジェクトとしてみなす。 */
+
+//DOM(ドム)
+/*ドキュメントオブジェクトモデル　(Document Object Model)の略で、webページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+
+//上記コードの意味
+//document.querySelector("h1").textContent= "自分の名前"
+
+//ドキュメントの中の（”h1”）タグを探してね
+//document.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent = "自分の名前"
