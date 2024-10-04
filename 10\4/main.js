4901c1094367eeb087fc4d581ed5dbb6c3544bf7
youye1291 <nkhtyuuya11@gmail.com>
2024-10-04 06:02:34 +0000

    １０月４日写経

---
+++ 10\4/main.js
@@ -0,0 +1,9 @@
+function errorInput(){
+const month = document.getElementById("month").value;
+const day = document.getElementById("day").value;
+if(month<1 || month>12){
+    alert("適切な値ではありません")
+    return
+}
+console.log("Month:",month,"Day:",day);
+}
