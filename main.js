d1b419ec831dd791cb5e759f7e79bd496e3595bf
youye1291 <nkhtyuuya11@gmail.com>
2025-01-16 05:21:27 +0000

    ボタンの追加

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 
 function ClickCounterGeme(){
     let count = 0;
+
     const gameContainer = document.getElementById("game-container");
 
     let button1 = document.createElement("button")
+
+    let button2 = document.createElement("button")
+    
+    let button3 = document.createElement("button")
+
     button1.textContent = "+1ボタン";
+
+    button2.textContent = "+10ボタン";
+
+    button3.textContent = "リセットボタン";
 
     let counter = document.createElement("p");
     counter.textContent = count;
@@ -14,7 +24,19 @@ function ClickCounterGeme(){
         counter.textContent = count;
     })
 
+    button2.addEventListener("click",function(){
+        count = count + 10;
+        counter.textContent = count;
+    })
+
+    counter.addEventListener("click",function(){
+        count = 0;
+        counter.textContent = count;
+    })
+
     gameContainer.appendChild(button1);
+    gameContainer.appendChild(button2);
+    gameContainer.appendChild(button3);
     gameContainer.appendChild(counter);
 }
 ClickCounterGeme();
